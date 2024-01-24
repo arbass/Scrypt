fa865080521f61d608efe9f0c355ad58f0bee760
arbass <13ar.bass@gmail.com>
2024-01-24 04:36:39 +0300

    updated: relevant case studies
    Now it detects, if there is no key stage, it hides the header.

--- src/utils/nested-products.ts
+++ src/utils/nested-products.ts
@@ -3,49 +3,38 @@
 export const nestedProduct = () => {
   const nestDiv = document.querySelector('.nested-src');
   if (nestDiv) {
-    const allProductEmbed = document.querySelectorAll('[has-content]');
-    allProductEmbed.forEach((el_productEmbed) => {
-      const currentCustomerCategory_el = el_productEmbed.getAttribute('customer-category');
+    // const allProductEmbed = document.querySelectorAll('[has-content]');
+    // allProductEmbed.forEach((el_productEmbed) => {
+    //   const currentCustomerCategory_el = el_productEmbed.getAttribute('customer-category');
 
-      const allNestWaiters = document.querySelectorAll('[nest-product-here]');
-      allNestWaiters.forEach((nestWaiter) => {
-        const currentCustomerCategory_waiter = nestWaiter.getAttribute('nest-product-here');
-        if (currentCustomerCategory_el === currentCustomerCategory_waiter) {
-          const closestTagToAppend = el_productEmbed.closest('.solutions-content_product-tag');
-          nestWaiter.appendChild(closestTagToAppend);
-        }
-      });
-      setTimeout(function () {
-        allNestWaiters.forEach((nestWaiter) => {
-          if (nestWaiter.childElementCount === 0) {
-            nestWaiter.classList.add('hide');
-            nestWaiter.previousElementSibling.classList.add('hide');
-          }
-        });
-      }, 100);
-    });
+    //   const allNestWaiters = document.querySelectorAll('[nest-product-here]');
+    //   allNestWaiters.forEach((nestWaiter) => {
+    //     const currentCustomerCategory_waiter = nestWaiter.getAttribute('nest-product-here');
+    //     if (currentCustomerCategory_el === currentCustomerCategory_waiter) {
+    //       const closestTagToAppend = el_productEmbed.closest('.solutions-content_product-tag');
+    //       nestWaiter.appendChild(closestTagToAppend);
+    //     }
+    //   });
+    //   setTimeout(function () {
+    //     allNestWaiters.forEach((nestWaiter) => {
+    //       if (nestWaiter.childElementCount === 0) {
+    //         nestWaiter.classList.add('hide');
+    //         nestWaiter.previousElementSibling.classList.add('hide');
+    //       }
+    //     });
+    //   }, 100);
+    // });
 
-    const allSectionDiscriptionWrapper = document.querySelectorAll(
-      '.solutions-content_text-wrapper'
-    );
-    allSectionDiscriptionWrapper.forEach((el) => {
-      const description = el.querySelector('.accent-m');
+    // const allSectionDiscriptionWrapper = document.querySelectorAll(
+    //   '.solutions-content_text-wrapper'
+    // );
+    // allSectionDiscriptionWrapper.forEach((el) => {
+    //   const description = el.querySelector('.accent-m');
 
-      if (description.classList.contains('w-dyn-bind-empty')) {
-        description.classList.add('hide');
-      }
-    });
-
-    const allCaseCardsWrappers = document.querySelectorAll(
-      '.solutions-content_relevant-grid.is-waiter'
-    );
-
-    allCaseCardsWrappers.forEach((el) => {
-      if (el.childElementCount === 0) {
-        el.classList.add('hide');
-        el.previousElementSibling.classList.add('hide');
-      }
-    });
+    //   if (description.classList.contains('w-dyn-bind-empty')) {
+    //     description.classList.add('hide');
+    //   }
+    // });
 
     fsAttributes.cmsnest.loading
       .then((result) => {
@@ -53,6 +42,18 @@ export const nestedProduct = () => {
 
         elements.forEach((element) => {
           element.removeAttribute('href');
+        });
+
+        //–––
+        const allCaseCardsWrappers = document.querySelectorAll(
+          '.solutions-content_relevant-grid.is-waiter'
+        );
+
+        allCaseCardsWrappers.forEach((el) => {
+          if (el.childElementCount === 0) {
+            el.classList.add('hide');
+            el.previousElementSibling.classList.add('hide');
+          }
         });
       })
       .catch((error) => {
